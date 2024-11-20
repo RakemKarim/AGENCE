69d9f65e62ffa7223f7e172c59b9468d87ad507d
Abderrahmane-Karim Rakem <abderrahmanekarim.rakem.etu@univ-lille.fr>
2024-11-20 02:22:28 +0100

    rendu

---
+++ classes/Vehicules.php
@@ -0,0 +1,32 @@
+<?php
+
+class Vehicules{
+    private string $marque;
+    private int  $immatriculation;
+    private string  $type;
+    private float  $prixParJour;
+    private boolean $disponibilite ;
+
+    public function __construct(string $marque, int $immatriculation, string  $type,float  $prixParJour){
+        $this->marque = $marque;
+        $this->prixParJour = $prixParJour;
+        $this->immatriculation = $immatriculation;
+        $this->marque = $marque;
+        $this->$disponibilite=true;
+    }
+    public function getId() {
+        return $this->id;
+    }
+    public function getId() {
+        return $this->id;
+    }
+ 
+    public function getStatut() {
+        return $this->disponibilite ? 'Disponible' : 'Indisponible';
+    }
+
+    public function setStatut($statut) {
+        $this->disponibilite = $statut;
+    }
+
+}
